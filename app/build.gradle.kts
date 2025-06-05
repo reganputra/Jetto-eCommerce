b669eda72e7d54dd6cfce4797b3643e5d705d71a
reganputra <ramadhanregan334@gmail.com>
2025-06-05 21:30:09 +0700

    feat: add compose dependencies to core ui and auth modules
    This commit adds the necessary dependencies for Jetpack Compose to the `core/ui` and `feature/auth` modules.
    It also adds the `core/ui` module as a dependency to the `app` module.
    The `compose` build feature has been enabled in both `core/ui` and `feature/auth` modules.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    // Modules
+    implementation(project(":core:ui"))
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- feature/auth/build.gradle.kts
+++ feature/auth/build.gradle.kts
@@ -30,13 +30,23 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

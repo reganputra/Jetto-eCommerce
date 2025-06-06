753c76052d21cc4e48ee2b2b2c373b429a9e3f7d
reganputra <ramadhanregan334@gmail.com>
2025-06-06 11:22:53 +0700

    feat: arrow back icon

--- feature/auth/src/main/java/com/example/auth/MainActivity.kt
+++ feature/auth/src/main/java/com/example/auth/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             JettoCommerceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Regan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
